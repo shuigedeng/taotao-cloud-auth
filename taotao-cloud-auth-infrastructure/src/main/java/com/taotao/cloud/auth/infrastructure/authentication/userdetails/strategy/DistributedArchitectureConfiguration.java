33d2ab3fff66f294f9cd96cefda6a4706909e3ea
shuigedeng <981376577@qq.com>
2026-01-06 17:57:36 +0800

    开启2026.01

--- taotao-cloud-auth-infrastructure/src/main/java/com/taotao/cloud/auth/infrastructure/authentication/userdetails/strategy/DistributedArchitectureConfiguration.java
+++ taotao-cloud-auth-infrastructure/src/main/java/com/taotao/cloud/auth/infrastructure/authentication/userdetails/strategy/DistributedArchitectureConfiguration.java
@@ -16,10 +16,9 @@
 
 package com.taotao.cloud.auth.infrastructure.authentication.userdetails.strategy;
 
-import com.taotao.cloud.auth.infrastructure.authentication.extension.social.handler.SocialAuthenticationHandler;
 import com.taotao.cloud.auth.infrastructure.authentication.userdetails.strategy.remote.RemotePermissionDetailsService;
 import com.taotao.cloud.auth.infrastructure.authentication.userdetails.strategy.remote.RemoteUserDetailsService;
-import com.taotao.cloud.sys.api.inner.UserApi;
+import com.taotao.cloud.sys.api.api.UserApi;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
