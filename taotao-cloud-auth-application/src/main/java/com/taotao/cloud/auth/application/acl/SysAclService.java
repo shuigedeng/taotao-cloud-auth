e955852550ca5687db7fa53ac4d1cf975532da64
shuigedeng <981376577@qq.com>
2026-02-06 14:05:01 +0800

    开启2026.03

--- taotao-cloud-auth-application/src/main/java/com/taotao/cloud/auth/application/acl/SysAclService.java
+++
@@ -1,4 +0,0 @@
-package com.taotao.cloud.auth.application.acl;
-
-public interface SysAclService {
-}
--- taotao-cloud-auth-common/src/main/java/com/taotao/cloud/auth/common/enums/BusinessResultEnum.java
+++
@@ -1,5 +0,0 @@
-package com.taotao.cloud.auth.common.enums;
-
-public enum BusinessResultEnum  {
-
-}
